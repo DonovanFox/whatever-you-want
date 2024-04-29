3aaf8cc2f0fd7edf3bd37b447dc4bf8e12acd0f7
Donovan Fox <you@example.com>
2024-04-29 09:33:20 -0500

    forrest gump

--- whatever/src/App.js
+++ whatever/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,18 +17,19 @@ function App() {
         >
           Learn React
         </a>
-      </header>
-      <p>
+      </header>*/}
+      <p className="App-header">
         UNLIMITED FREE WEBSITES (loaded html on a browser)
-      </p>
-      <a
-          className="Song-link"
+        <iframe width="784" height="441" src="https://www.youtube.com/embed/22W83FgIX54?si=UZAr_ejMxOKQ3SBq" title="YouTube video player" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <a
+          className="App-link"
           href="jetpack.flac"
           target="_blank"
           rel="noopener noreferrer"
         download>
           MUSIC
         </a>
+      </p>
     </div>
   );
 }
