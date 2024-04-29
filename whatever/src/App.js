3d479789e65af068af3b47968e6413a191095b9e
Donovan Fox <you@example.com>
2024-04-29 10:20:45 -0500

    making nice look

--- whatever/src/App.js
+++ whatever/src/App.js
@@ -22,13 +22,15 @@ function App() {
         UNLIMITED FREE WEBSITES (loaded html on a browser)
         <iframe width="784" height="441" src="https://www.youtube.com/embed/22W83FgIX54?si=UZAr_ejMxOKQ3SBq" title="YouTube video player" frameborder="10" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         <a
-          className="App-link"
+          className="Song-link"
           href="jetpack.flac"
           target="_blank"
           rel="noopener noreferrer"
         download>
-          MUSIC
+        <p className="With-margins">MUSIC</p>
         </a>
+        Undertale
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=ubLatubBziyx3uJO&amp;list=PLpJl5XaLHtLX-pDk4kctGxtF4nq6BIyjg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </p>
     </div>
   );
