a571127ac383c58ed9afdc6d1a86753121a35604
Donovan Fox <you@example.com>
2024-05-01 09:34:35 -0500

    ADDING CONTENT

--- whatever/src/App.js
+++ whatever/src/App.js
@@ -31,6 +31,22 @@ function App() {
         </a>
         Undertale
         <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=ubLatubBziyx3uJO&amp;list=PLpJl5XaLHtLX-pDk4kctGxtF4nq6BIyjg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        The Planets
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/Isic2Z2e2xs?si=VHpeiw9q8jSF7YyR" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        French "people"
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=PsnU6w_TWAbyBsZO&amp;list=PLi4VqsCqAXj_BSQPFhA7hqbR-pfnCgdSv" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        David Bowie
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=mkEm0LD6gIyHgsd_&amp;list=PL6ogdCG3tAWiH0E0OF6d97Ahfrq2xz8fj" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        ASDFMOVIES
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=bg9FEjo2leXYLl7R&amp;list=PLrO_4wZHAWnVmPGftDPKq9ZtFAV1S2K4P" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        Metal Gear?
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/z11-EYv5QbA?si=1ZUyop8ryn3APlNr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        Rising Vocals and Instrumentals
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=5YvnI9_5DUNDsEDH&amp;list=PL1tFM4hAoC6VGoSUQBWXdykwzjT96cYeb" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        Who is Brandenburg?
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/HTm2kEAfo8o?si=j0tQXkwbL75C6CJ0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        Bible Studies
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/videoseries?si=p515kNAzQ2qZaXjR&amp;list=PLhPp-QAUKF_iiixYtOGmynw-BMEju-EWo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </p>
     </div>
   );
